358cc0345b59ee0cabe9d1d2ad38c811e744cd65
Arulselvan Madhavan <arulselvan1234@gmail.com>
2019-05-05 22:59:14 -0700

    Doin exercises

--- okasaki-pfds/chapter2/bin/exercises.ml
+++ okasaki-pfds/chapter2/bin/exercises.ml
@@ -1,12 +1,16 @@
 open Chapter2.Stack
+open Chapter2.Tree
 
 module List_Helpers (L : Stack with type 'a t = 'a list) = struct
   let rec ( ++ ) xs ys = match xs with [] -> ys | x :: xs -> x :: (xs ++ ys)
 
   let rec update = function
-    | [], _, _ -> failwith "subscript"
-    | _ :: xs, 0, y -> y :: xs
-    | x :: xs, i, y -> x :: update (xs, i - 1, y)
+    | [], _, _ ->
+        failwith "subscript"
+    | _ :: xs, 0, y ->
+        y :: xs
+    | x :: xs, i, y ->
+        x :: update (xs, i - 1, y)
 
   let rec suffixes = function [] -> [] | _ :: ys as xs -> xs :: suffixes ys
 end
--- okasaki-pfds/chapter2/lib/tree.ml
+++ okasaki-pfds/chapter2/lib/tree.ml
@@ -9,15 +9,27 @@ module UnbalancedSet (O : Ordered) = struct
 
   let empty = E
 
+  (* Takes 2d comparisons in worst case *)
   let rec member = function
-    | _, E -> false
+    | _, E ->
+        false
     | x, T (a, y, b) ->
         if O.lt (x, y) then member (x, a)
         else if O.lt (y, x) then member (x, b)
         else true
 
+  let member_less_comparisons x s =
+    let rec go = function
+      | y, E ->
+          if O.eq (x, y) then true else false
+      | y, T (a, z, b) ->
+          if O.lt (x, z) then go (z, a) else go (y, b)
+    in
+    go (x, s)
+
   let rec insert = function
-    | x, E -> T (E, x, E)
+    | x, E ->
+        T (E, x, E)
     | x, T (a, y, b) ->
         if O.lt (x, y) then T (insert (x, a), y, b)
         else if O.lt (y, x) then T (a, y, insert (x, b))

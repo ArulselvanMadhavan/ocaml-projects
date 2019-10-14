2c0a4f22c90b1613c90ba7010cfa1dc96a846d20
Arulselvan Madhavan <arulselvan1234@gmail.com>
2019-10-13 22:06:59 -0700

    Using Command.Async

--- concurrent-programming/main.ml
+++ concurrent-programming/main.ml
@@ -22,6 +22,28 @@ let run () =
   in
   ignore (host_and_port : (Socket.Address.Inet.t, int) Tcp.Server.t Deferred.t)
 
+let run ~uppercase ~port =
+  let host_and_port =
+    Tcp.Server.create
+      ~on_handler_error:`Raise
+      (Tcp.Where_to_listen.of_port port)
+      (fun _addr r w ->
+         Pipe.transfer (Reader.pipe r) (Writer.pipe w)
+           ~f:(if uppercase then String.uppercase else Fn.id))
+  in
+  ignore (host_and_port : (Socket.Address.Inet.t, int) Tcp.Server.t Deferred.t);
+  Deferred.never ()
+    
 let () =
-  run ();
-  never_returns (Scheduler.go ())
+  Command.async_spec
+    ~summary:"Start an echo server"
+    Command.Spec.(
+      empty
+      +> flag "-uppercase" no_arg
+        ~doc:"Convert to uppercase before echoing back"
+      +> flag "-port" (optional_with_default 8765 int)
+        ~doc:"Port to listen on"
+    )
+    (fun uppercase port () -> run ~uppercase ~port)
+  |> Command.run
+       
